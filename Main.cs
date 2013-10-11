485a7f1a2e0e40b81c979c7b797b2db93d8f01b8
rubi15 <rubi_casarrubias@hotmail.com>
2013-10-11 15:29:13 -0500

    inicio practica

---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace Practica_2
+{
+	public class Main{
+		
+		
+		public Main(){
+			
+		}
+		
+		public void iniciar(){
+			Console.Clear();
+			
+			int h=DateTime.Now.Hour;
+			int m=DateTime.Now.Minute;
+			int s=DateTime.Now.Second;
+
+			for(int hora=h; hora<= 23; hora++)
+				for(int minuto=m; minuto<= 59; minuto++)
+					for(int segundo=s; segundo<= 59; segundo++){
+						Console.WriteLine(hora + ":" + minuto + ":" + segundo);
+						System.Threading.Thread.Sleep(1000);
+						Console.Clear();
+				}
+		}
+
+		public void cronometro(){
+			Console.Clear();
+
+			for(int hora=0; hora<= 23; hora++)
+				for(int minuto=0; minuto<= 59; minuto++)
+					for(int segundo=0; segundo<= 59; segundo++){
+						Console.WriteLine(hora + ":" + minuto + ":" + segundo);
+						System.Threading.Thread.Sleep(1000);
+						Console.Clear();
+					}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace Practica_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Console.WriteLine ("\n1.- Reloj");
+			Console.WriteLine ("2.- Cronómetro ");
+			Console.Write ("\n ** Seleccione una opcion: ** \n");
+			Main M=new Main();
+
+			int opc = Convert.ToInt32(Console.ReadLine());
+			switch(opc){
+			case 1:
+				{
+					M.iniciar();
+				}break;
+
+			case 2:
+				{
+					M.cronometro();
+				}break;
+
+           default : Console.WriteLine("La opcion que ingreso No es válida.");
+				break;
+			}
+		}
+	}
+}
